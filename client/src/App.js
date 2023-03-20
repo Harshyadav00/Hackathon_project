7e875f6a8c98f4fda4ba0356cf54bc6572ac6b71
Riya <21cs74@ecajmer.ac.in>
2023-03-20 17:16:40 +0530

    riya

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages';
 import HospitalLogin from './pages/hospitalLogin';
 import PatientLogin from './pages/patientLogin';
 import PatientOtp from './pages/patientOtp';
+import History from './pages/History';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 		<Route path='/hospitalLogin' element={<HospitalLogin/>} />
 		<Route path='/patientLogin' element={<PatientLogin/>} />
 		<Route path='/patientotp' element={<PatientOtp/>}/>
+		<Route path='/history' element={<History/>}/>
 		{/* <Route path='/events' component={Events} /> */}
 	</Routes>
 	</Router>
